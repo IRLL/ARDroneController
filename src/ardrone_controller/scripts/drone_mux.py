657e40a8d30dc66b9e144e64385098a720a65230
James Irwin <irwineffect@gmail.com>
2016-03-15 20:13:29 -0700

    converted tabs to spaces

--- src/ardrone_controller/scripts/drone_mux.py
+++ src/ardrone_controller/scripts/drone_mux.py
@@ -3,7 +3,7 @@
 """
 Author: James Irwin (james.irwin@wsu.edu)
 Description:
-	Example code for getting started with the ardrone
+    Example code for getting started with the ardrone
 """
 
 import rospy
@@ -13,63 +13,63 @@ from geometry_msgs.msg import Pose
 from std_msgs.msg import Empty #send empty message for takeoff and landing
 
 class DroneMux:
-	def __init__(self):
-		self.teleop_sub = rospy.Subscriber('/cmd_vel_mux/input/teleop', Twist, self.teleop_callback)
-		self.location_sub = rospy.Subscriber('/ardrone/location', Pose, self.location_callback)
-		#create message publisher for sending drone movement commands
-		self.output_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-		self.location = Pose()
-		self.bounds = rospy.get_param("/bounds")
+    def __init__(self):
+        self.teleop_sub = rospy.Subscriber('/cmd_vel_mux/input/teleop', Twist, self.teleop_callback)
+        self.location_sub = rospy.Subscriber('/ardrone/location', Pose, self.location_callback)
+        #create message publisher for sending drone movement commands
+        self.output_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.location = Pose()
+        self.bounds = rospy.get_param("/bounds")
 
-	def teleop_callback(self, twist):
-		#check if we're within bounds
-		if self.in_bounds():
-			print 'yes'
-			output = twist
-		else: #we need to correct
-			print 'no'
-		 	output = self.get_correction()
+    def teleop_callback(self, twist):
+        #check if we're within bounds
+        if self.in_bounds():
+            print 'yes'
+            output = twist
+        else: #we need to correct
+            print 'no'
+            output = self.get_correction()
 
-		self.output_pub.publish(output)
+        self.output_pub.publish(output)
 
-	def in_bounds(self):
-		print self.location.position.z
-		if self.location.position.z < 3:
-			return True
-		else:
-			return False
+    def in_bounds(self):
+        print self.location.position.z
+        if self.location.position.z < 3:
+            return True
+        else:
+            return False
 
-	def get_correction(self):
-		twist = Twist()
-		twist.linear.z = -0.05
-		return twist
+    def get_correction(self):
+        twist = Twist()
+        twist.linear.z = -0.05
+        return twist
 
-	
-	def location_callback(self, location):
-		self.location = location
-	
+    
+    def location_callback(self, location):
+        self.location = location
+    
 
 #callback function that gets run when node shuts down
 def shutdown_callback():
-	print "shutting down..."
-	drone_land_pub.publish(Empty()) #make the drone land
-	print "done!"
+    print "shutting down..."
+    drone_land_pub.publish(Empty()) #make the drone land
+    print "done!"
 
 
 if __name__ == "__main__":
-	rospy.init_node("drone_mux")
-	#ardrone uses specialized topics for landing and taking off
-	drone_takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=1)
-	drone_land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=1)
-	mux = DroneMux()
+    rospy.init_node("drone_mux")
+    #ardrone uses specialized topics for landing and taking off
+    drone_takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=1)
+    drone_land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=1)
+    mux = DroneMux()
 
-	#register callback function to land drone if we kill the program
-	rospy.on_shutdown(shutdown_callback) 
+    #register callback function to land drone if we kill the program
+    rospy.on_shutdown(shutdown_callback) 
 
-	rospy.sleep(1) #wait for a second to wait for node to fully connect
-	drone_takeoff_pub.publish(Empty()) #command drone to takeoff
+    rospy.sleep(1) #wait for a second to wait for node to fully connect
+    drone_takeoff_pub.publish(Empty()) #command drone to takeoff
 
-	#this function loops and waits for node to shutdown
-	#all logic happens in the image_callback function
-	rospy.spin()
+    #this function loops and waits for node to shutdown
+    #all logic happens in the image_callback function
+    rospy.spin()
 
--- src/ardrone_controller/scripts/location_gazebo.py
+++ src/ardrone_controller/scripts/location_gazebo.py
@@ -3,7 +3,7 @@
 """
 Author: James Irwin (james.irwin@wsu.edu)
 Description:
-	Publishes Gazebo pose information to /ardrone/location
+    Publishes Gazebo pose information to /ardrone/location
 """
 
 import rospy
@@ -11,28 +11,28 @@ from geometry_msgs.msg import Pose
 from gazebo_msgs.msg import ModelStates
 
 class Location:
-	def __init__(self):
-		self.location_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.location_callback)
-		#create message publisher for sending drone movement commands
-		self.output_pub = rospy.Publisher('/ardrone/location', Pose, queue_size=1)
-		self.count = 0
+    def __init__(self):
+        self.location_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.location_callback)
+        #create message publisher for sending drone movement commands
+        self.output_pub = rospy.Publisher('/ardrone/location', Pose, queue_size=1)
+        self.count = 0
 
-	def location_callback(self, location):
-		#gazebo publishes at 100hz, only publish every 5th message to get 20hz
-		self.count += 1
-		if self.count > 4:
-			quadrotor_index = location.name.index('quadrotor')
-			quadrotor_pose = location.pose[quadrotor_index]
-			self.output_pub.publish(quadrotor_pose)
-			self.count = 0
-	
+    def location_callback(self, location):
+        #gazebo publishes at 100hz, only publish every 5th message to get 20hz
+        self.count += 1
+        if self.count > 4:
+            quadrotor_index = location.name.index('quadrotor')
+            quadrotor_pose = location.pose[quadrotor_index]
+            self.output_pub.publish(quadrotor_pose)
+            self.count = 0
+    
 
 if __name__ == "__main__":
-	rospy.init_node("location")
+    rospy.init_node("location")
 
-	location = Location()
+    location = Location()
 
-	#this function loops and waits for node to shutdown
-	#all logic happens in the image_callback function
-	rospy.spin()
+    #this function loops and waits for node to shutdown
+    #all logic happens in the image_callback function
+    rospy.spin()
 
