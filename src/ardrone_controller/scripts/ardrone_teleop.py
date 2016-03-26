f649614c99b81c5a20a3cfdc47efad5f9b17cf13
James Irwin <irwineffect@gmail.com>
2016-03-25 22:16:46 -0700

    added ability to land and takeoff from ps3 controller

--- src/ardrone_controller/scripts/ardrone_teleop.py
+++ src/ardrone_controller/scripts/ardrone_teleop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
 from ardrone_controller.msg import ps3
 
 
@@ -9,11 +10,13 @@ class ardrone_teleop():
         self.controller_state = ps3()
         self.ps3_sub = rospy.Subscriber('/ps3', ps3, self.ps3_callback)
         self.twist_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+        self.takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=1)
+        self.land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=1)
     
     def ps3_callback(self, msg):
         self.controller_state = msg
     
-    def send_twist(self):
+    def send_command(self):
         current_state = self.controller_state
 
         cmd = Twist()
@@ -24,6 +27,14 @@ class ardrone_teleop():
         #logic for controlling height
         cmd.linear.z = -current_state.trigL2 - -current_state.trigR2
 
+        if(current_state.btnSquare): #land	
+            self.land_pub.publish(Empty())
+            return
+            
+        if(current_state.btnTriangle): #takeoff
+            self.takeoff_pub.publish(Empty())
+            return
+            
 
         self.twist_pub.publish(cmd)
 
@@ -36,7 +47,7 @@ if __name__ == "__main__":
     r = rospy.Rate(20)
 
     while not rospy.is_shutdown():
-        teleop.send_twist()
+        teleop.send_command()
         r.sleep()
 
 
